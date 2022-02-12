63a6640d1bf327c08924d0a5031754e42b43ca3c
Minhhkm <96058996+Minhhkm@users.noreply.github.com>
2022-02-12 11:30:56 +0900

    Update lotto.js

--- lotto/lotto.js
+++ lotto/lotto.js
@@ -8,5 +8,25 @@ while (candidate.length > 0) {
 }
 console.log(shuffle);
 const winBalls = shuffle.slice(0,6).sort((a,b)=> a - b);
-const bonus = shuffle[6]; //배열 6이 7번ㅉ
+const bonus = shuffle[6]; //배열 6이 7번째 보너스 숫자
 console.log(winBalls, bonus);
+const $result = document.querySelector('#result');
+const $bonus = document.querySelector('#bonus');
+
+const showball = (number,$target) => {
+  const $ball = document.createElement('div');
+  $ball.className = 'ball';
+  $ball.textContent = number;
+  $target.appendChild($ball);
+};
+
+//
+for (let i=0 ; i < 6 ; i++) {
+  setTimeout(() => {
+    showball(winBalls[i], $result);
+  }, (i+1)*1000);
+}
+
+setTimeout(()=>{    //setTimout(()=> {}, 5000=5초)
+  showball(bonus,$bonus);
+},7000);
