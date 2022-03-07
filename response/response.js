74887da16db4e2d3e8ec61cf9c803a35b6b903bd
Minhhk <alszn2222@gmail.com>
2022-03-07 23:17:45 +0900

    update

--- response/response.js
+++ response/response.js
@@ -1,18 +1,37 @@
 const $screen = document.querySelector('#screen');
 const $result = document.querySelector('#result');
+let startTime;
+let endTime;
+const records = [];
+let timeoutId;
 
 $screen.addEventListener('click', function () {
-  if ($screen.classList.contains('waiting')) { // 대기 화면
+  if ($screen.classList.contains('waiting')) { // 대기 화면(파랑)
     $screen.classList.remove('waiting');
     $screen.classList.add('ready');
     $screen.textContent = '초록색이 되면 클릭하세요';
-    setTimeout(function () {
+    timeoutId = setTimeout(function () {
+      startTime = new Date ();
       $screen.classList.remove('ready');
       $screen.classList.add('now');
       $screen.textContent = '클릭하세요!';
     }, Math.floor(Math.random() * 1000) + 2000); // 2000~3000 사이 수
     
-  } else if ($screen.classList.contains('ready')) { // 준비 화면
-  } else if ($screen.classList.contains('now')) { // 클릭 화면
+  } else if ($screen.classList.contains('ready')) { // 준비 화면(빨강)
+    clearTimeout(timeoutId);
+    $screen.classList.remove('ready');
+    $screen.classList.add('waiting');
+    $screen.textContent = '성급하시네요^ 다시 선택하세요';
+  } else if ($screen.classList.contains('now')) { // 클릭 화면(초록)
+    endTime = new Date ();
+    const current = endTime - startTime;
+    records.push(current);
+    const average = records.reduce((a, c) => a + c) / records.length;
+    $result.textContent = `현재 ${current}ms, 평균: ${average}ms`;
+    startTime = null;
+    endTime = null;
+    $screen.classList.remove('now');
+    $screen.classList.add('waiting');
+    $screen.textContent = '클릭해서 시작';
   }
 });
